94000522735d27cde9d62e0dacf109c84d9927e6
StargazerCatsriel <danobrovold@gmail.com>
2025-12-02 13:17:04 +0100

    Add Final Nav Bar

--- podcast_lp/app/page.tsx
+++ podcast_lp/app/page.tsx
@@ -1,14 +1,10 @@
 import Image from "next/image";
+import Navbar from "./components/navbar";
 
 export default function Home() {
   return (
-    <div className="bgImage">
-      <div className="flex justify-center items-center h-screen">
-        <div className="flex items-center flex-col bg-white p-10 border-2 border-green-600">
-          <h1 className="text-xl sm:text-4xl font-bold">The Landing Page w/ Barry Block</h1>
-          <h2 className="text-md sm:text-2xl">Episode #1 - The Malware Episode </h2>
-        </div>
-      </div>
+    <div>
+      <Navbar></Navbar>
     </div>  
   );
 }
