36a8009b0061605c5f3d2db46933c7498cbfbbe2
StargazerCatsriel <danobrovold@gmail.com>
2025-12-02 14:17:20 +0100

    Add Top Info

--- podcast_lp/app/components/navbar.tsx
+++ podcast_lp/app/components/navbar.tsx
@@ -3,14 +3,15 @@ import Image from "next/image";
 export default function Navbar(){
     return (
         <nav>
-            <div className="sm:w-1/3 justify-center sm:justify-normal flex items-center p-5">
-                <Image src={"/michaelPhone.png"} height={100} width={100} alt="michael phone"></Image>
-                <div>
-                    <h1 className="text-3xl font-bold text-center">The Landing Page</h1>
-                    <h2 className="text-xl text-center">A Security Podcast</h2>
+                <div className="sm:w-1/3 justify-center sm:justify-normal flex items-center p-5">
+                    <Image src={"/michaelPhone.png"} height={100} width={100} alt="michael phone"></Image>
+                    <div>
+                        <h1 className="text-3xl font-bold text-center">The Landing Page</h1>
+                        <h2 className="text-xl text-center">A InfoSec Podcast</h2>
+                    </div>
                 </div>
-            </div>
-            <hr></hr>
+
+                <hr></hr>
         </nav>
     )
 }
--- podcast_lp/app/page.tsx
+++ podcast_lp/app/page.tsx
@@ -5,6 +5,14 @@ export default function Home() {
   return (
     <div>
       <Navbar></Navbar>
+
+      <div className="text-center p-5">
+        <h3 className="text-2xl sm:text-4xl font-bold p-5">ABOUT THE LANDING PAGE</h3>
+        <p className="text-md sm:text-xl">Hosted by Barry Block, The Landing Page is a beginner-oriented Cyber Security Podcast.</p>
+        <p className="text-md sm:text-xl">Every Episode covers a different aspect of Cyber Security, in an effort to make Cyber Security easier to learn.</p>
+        <p className="text-md sm:text-xl">Learn more from experts in the field, and have fun along the way.</p>
+      </div>
+
     </div>  
   );
 }
